e0233b5568935532ac08bc768d61b4f7b3870e05
Zhi Syuen See <syuensee@usc.edu>
2022-03-25 12:10:47 -0700

    added temp buttons on MainActivity to navigate to pages

--- app/src/main/java/com/example/uscrecapp/MainActivity.java
+++ app/src/main/java/com/example/uscrecapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +14,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent gym = new Intent(this, gymSlots.class);
-        startActivity(gym);
+        Button button = findViewById(R.id.summaryNav);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent sumNav = new Intent(view.getContext(), SummaryPage.class);
+                startActivity(sumNav);
+
+            }
+        });
+
+        Button button2 = findViewById(R.id.gymNav);
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent gymNav = new Intent(view.getContext(), gymSlots.class);
+                startActivity(gymNav);
+
+            }
+        });
+
+
+
 
     }
 }
