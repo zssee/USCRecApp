b73ac68100f39151e3f1739a05fcede800d1dd89
Kelly Ma <kjma@usc.edu>
2022-03-25 17:16:20 -0700

    navbar shows up but not functional

--- app/src/main/java/com/example/uscrecapp/MainActivity.java
+++ app/src/main/java/com/example/uscrecapp/MainActivity.java
@@ -1,18 +1,37 @@
 package com.example.uscrecapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                switch(item.getItemId()){
+                    case R.id.person:
+
+                        break;
+                    case R.id.home:
+                        break;
+                }
+                return false;
+            }
+        });
 
         Button button = findViewById(R.id.summaryNav);
         button.setOnClickListener(new View.OnClickListener() {
@@ -38,8 +57,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
     }
+
+
 }
