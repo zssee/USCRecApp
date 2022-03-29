8215c89bd735416a2eabfd1a49607ad0b0540884
Elle Szabo <eszabo@usc.edu>
2022-03-28 19:01:27 -0700

    runs

--- app/src/main/java/com/example/uscrecapp/map.java
+++ app/src/main/java/com/example/uscrecapp/map.java
@@ -24,28 +24,19 @@ public class map extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ImageButton l = (ImageButton) findViewById(R.id.lyon);
-        l.setX(60);
-        l.setY(100);
+        setContentView(R.layout.activity_map2);
+//        ImageButton l = (ImageButton) findViewById(R.id.lyon);
+//        l.setX(60);
+//        l.setY(100);
 
         ImageButton s = (ImageButton) findViewById(R.id.swim);
         ImageButton v = (ImageButton) findViewById(R.id.village);
         binding = ActivityMap2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
-
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_map2);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
