0c99d60ed183b8518acef8bc9d985c47a46bf199
Elle Szabo <eszabo@usc.edu>
2022-03-28 17:22:14 -0700

    loading but cannot center on a coordinate

--- app/src/main/java/com/example/uscrecapp/MainActivity.java
+++ app/src/main/java/com/example/uscrecapp/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.uscrecapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.CameraUpdate;
 
 // Implement OnMapReadyCallback.
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    LatLng center = new LatLng(34.022350,-118.285118);
+    LatLng lyons = new LatLng(34.022970,-118.284580);
+    LatLng swim = new LatLng(34.022970,-118.284580);
+    LatLng village = new LatLng(34.024550,-118.285630);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +27,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Set the layout file as the content view.
         setContentView(R.layout.activity_main);
 
-        // Get a handle to the fragment and register the callback.
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
         SupportMapFragment mapFragment = SupportMapFragment.newInstance();
         getSupportFragmentManager()
                 .beginTransaction()
@@ -33,8 +37,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     // Get a handle to the GoogleMap object and display marker.
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(center));
+        Log.d("idk", "executed");
+//        googleMap.addMarker(new MarkerOptions()
+//                .position(lyons)
+//                .title("lyons"));
     }
 }
